cc833d078e9099eef3f63e1889ade0de590d8bdc
ilaidasez <168421258+ilaidasez@users.noreply.github.com>
2024-12-10 16:18:51 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+let slider = document.querySelector('.slider');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+const slides = Array.from(slider.querySelectorAll('.slider .text'));
+const slideCount = slides.length;
+let slideIndex = 0;
+
+
+prevButton.addEventListener('click', showPreviousSlide);
+nextButton.addEventListener('click', showNextSlide);
+
+
+function showPreviousSlide() {
+    slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+    updateSlider();
+}
+
+
+function showNextSlide() {
+    slideIndex = (slideIndex + 1) % slideCount;
+    updateSlider();
+}
+
+function updateSlider() {
+    slides.forEach((slide, index) => {
+        if (index === slideIndex) {
+            slide.style.display = 'flex';
+        } else {
+            slide.style.display = 'none';
+        }
+    })
+}
+
+updateSlider();
